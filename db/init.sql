92c576dcb92722c8b44383406b36b33316acfd08
Thomas Pesendorfer <pesendorfer@s2032.rootserver.io>
2024-10-29 22:05:35 +0100

    Added initial POINTs to db/init.sql

--- db/init.sql
+++ db/init.sql
@@ -4,3 +4,10 @@ CREATE TABLE IF NOT EXISTS locations (
   geom GEOMETRY(Point, 4326)
 );
 
+INSERT INTO locations (name, geom) VALUES 
+('GEOINFO Applications AG', ST_GeomFromText('POINT(9.2892 47.3896)', 4326)),
+('Th. Pesendorfer', ST_GeomFromText('POINT(9.3349 47.411)', 4326)),
+('Café am Ring', ST_GeomFromText('POINT(9.374 47.4265)', 4326)),
+('Kaffeehaus St. Gallen', ST_GeomFromText('POINT(9.3836 47.4251)', 4326)),
+('Kafi Franz', ST_GeomFromText('POINT(9.382 47.4246)', 4326)),
+('Gnuss', ST_GeomFromText('POINT(9.3802 47.4244)', 4326));
